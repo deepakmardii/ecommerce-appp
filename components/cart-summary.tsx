46bfaab920044d554c4e76c99dce2d188b432acc
Deepak Mardi <67671892+deepakmardii@users.noreply.github.com>
2023-08-03 02:04:53 +0530

    .

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -36,7 +36,7 @@ export function CartSummary() {
   return (
     <section
       aria-labelledby="summary-heading"
-      className="mt-16 rounded-lg border-2 border-gray-200 bg-gray-50 px-4 py-6 shadow-md dark:border-gray-900 dark:bg-black sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+      className="px-4 py-6 mt-16 border-2 border-gray-200 rounded-lg shadow-md bg-gray-50 dark:border-gray-900 dark:bg-black sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
       <h2 id="summary-heading" className="text-lg font-medium">
         Order summary
@@ -47,25 +47,25 @@ export function CartSummary() {
           <dt className="text-sm">Subtotal</dt>
           <dd className="text-sm font-medium">{formattedTotalPrice}</dd>
         </div>
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
+        <div className="flex items-center justify-between pt-4 border-t border-gray-200 dark:border-gray-600">
           <dt className="flex items-center text-sm">
             <span>Shipping estimate</span>
           </dt>
           <dd className="text-sm font-medium">
-            {formatCurrencyString({ value: shippingAmount, currency: "USD" })}
+            {formatCurrencyString({ value: shippingAmount, currency: "INR" })}
           </dd>
         </div>
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4 dark:border-gray-600">
+        <div className="flex items-center justify-between pt-4 border-t border-gray-200 dark:border-gray-600">
           <dt className="text-base font-medium">Order total</dt>
           <dd className="text-base font-medium">
-            {formatCurrencyString({ value: totalAmount, currency: "USD" })}
+            {formatCurrencyString({ value: totalAmount, currency: "INR" })}
           </dd>
         </div>
       </dl>
 
       <div className="mt-6">
         <Button onClick={onCheckout} className="w-full" disabled={isDisabled}>
-          {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          {isLoading && <Loader2 className="w-4 h-4 mr-2 animate-spin" />}
           {isLoading ? "Loading..." : "Checkout"}
         </Button>
       </div>
